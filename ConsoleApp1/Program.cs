e88ea31affb0cb835753a273d662b5ebafe1107f
Averi Yu / Zhizhen Yu <73741014+averi-u@users.noreply.github.com>
2025-10-15 22:49:35 -0700

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PasswordGenerator;
+
+Console.WriteLine("Hello, World!");
+
+var pwd = new Password();
+var password = pwd.Next();
+
+Console.WriteLine(password.ToString());
