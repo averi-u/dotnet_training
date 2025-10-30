57ccf16448aecc8327cce208c92b7d048d9a024f
Averi Yu / Zhizhen Yu <73741014+averi-u@users.noreply.github.com>
2025-10-29 19:18:26 -0700

    successfully set up GraphQL

--- graphql_demo_app/Program.cs
+++ graphql_demo_app/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using HotChocolate.AspNetCore;
+using HotChocolate.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddProjections()
+    .AddFiltering()
+    .AddSorting();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGraphQL(); // endpoint: /graphql
 app.Run();
+
+public class Query
+{
+    public string Hello() => "Hello from GraphQL in .NET!";
+}
